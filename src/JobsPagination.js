53d240b3479fcff3292bd4a876671ce11dcce4b8
Joe DeSoto <joe.desoto@leaselabs.com>
2021-03-15 14:32:28 -0700

    Update: adds pagination conditionals so that duplicate pagination numbers do not show.
    Also adds pagination ellipses to be able to navigate back to
    page 1.

--- src/JobsPagination.js
+++ src/JobsPagination.js
@@ -5,7 +5,9 @@ export default function JobsPagination({ page, setPage }) {
   return (
     <Pagination>
       {page !== 1 && <Pagination.Prev />}
-      {page !== 1 && <Pagination.Item>{page - 1}</Pagination.Item>}
+      {page !== 1 && <Pagination.Item>1</Pagination.Item>}
+      {page > 2 && <Pagination.Ellipsis />}
+      {page > 2 && <Pagination.Item>{page - 1}</Pagination.Item>}
       <Pagination.Item>{page}</Pagination.Item>
       <Pagination.Item>{page + 1}</Pagination.Item>
       {page !== page.length - 1 && <Pagination.Next />}
