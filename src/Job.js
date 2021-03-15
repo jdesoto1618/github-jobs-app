52c57f37e902e2240b9ceeaf0602e95bffbad1f4
Joe DeSoto <joe.desoto@leaselabs.com>
2021-03-15 13:34:46 -0700

    Update: adds bootstrap badge component so that job types and job locations can be shown and styled.

--- src/Job.js
+++ src/Job.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Badge } from 'react-bootstrap';
 
 export default function Job({ job }) {
   return (
@@ -13,6 +13,12 @@ export default function Job({ job }) {
             <Card.Subtitle className='text-muted mb-2'>
               {new Date(job.created_at).toLocaleDateString()}
             </Card.Subtitle>
+            <Badge variant='secondary' className='mr-2'>
+              {job.type}
+            </Badge>
+            <Badge variant='secondary'>
+              {job.location}
+            </Badge>
           </div>
         </div>
       </Card.Body>
