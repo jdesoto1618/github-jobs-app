87a196971c6c52a9482b91db620d0977990f3eb5
Joe DeSoto <joe.desoto@leaselabs.com>
2021-03-15 11:27:48 -0700

    Update: creates custom hook js file for getting all the info for jobs from the github jobs api.
    Creates Bootstrap container within app.js to hold this info.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import useFetchJobs from './useFetchJobs';
+import { Container } from 'react-bootstrap';
 
 function App() {
-  return <h1>Hello World</h1>
+  const { jobs, loading, error } = useFetchJobs()
+  
+  return (
+    <Container>
+      
+    </Container>
+  )
 }
 
 export default App;
---
+++ src/useFetchJobs.js
@@ -0,0 +1,4 @@
+// hook for getting all the different yobs from Github
+export default function useFetchJobs(params, page) {
+  
+}
