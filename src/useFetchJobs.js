73c4c64580091b86089e4dd90d939b0ba824c70b
Joe DeSoto <joe.desoto@leaselabs.com>
2021-03-15 14:45:29 -0700

    Update: adds another action to the ACTIONS const for checking if something has a next page.
    Passes this action in as a case within the reducer function.

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -5,7 +5,8 @@ import axios from 'axios';
 const ACTIONS = {
   MAKE_REQUEST: 'make-request',
   GET_DATA: 'get-data',
-  ERROR: 'error'
+  ERROR: 'error',
+  UPDATE_HAS_NEXT_PAGE: 'update-has-next-page'
 }
 
 const BASE_URL = 'https://secret-ocean-49799.herokuapp.com/https://jobs.github.com/positions.json'
@@ -18,6 +19,8 @@ function reducer(state, action) {
       return { ...state, loading: false, jobs: action.payload.jobs }
     case ACTIONS.ERROR:
       return { ...state, loading: false, error: action.payload.error, jobs: [] }
+    case ACTIONS.UPDATE_HAS_NEXT_PAGE:
+      return { ...state, hasNextPage: action.payload.hasNextPage }
     default:
       return state
   }
