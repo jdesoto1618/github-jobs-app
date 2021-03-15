661fed6583bd6688d25f046a903af885562b9850
Joe DeSoto <joe.desoto@leaselabs.com>
2021-03-15 12:21:16 -0700

    Update: Adds code to useEffect function within custom hook to handle each type of action. Dispatch called on make request, get data, and error.
    Returns the state of the application instead of hardcoded info.

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -8,6 +8,8 @@ const ACTIONS = {
   ERROR: 'error'
 }
 
+const BASE_URL = 'https://jobs.github.com/positions.json'
+
 function reducer(state, action) {
   switch (action.type) {
     case ACTIONS.MAKE_REQUEST:
@@ -26,13 +28,15 @@ export default function useFetchJobs(params, page) {
   const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
 
   useEffect(() => {
-    displatch({ type: MAKE_REQUEST }),
-    axois.get()
+    dispatch({ type: ACTIONS.MAKE_REQUEST })
+    axios.get(BASE_URL, {
+      params: { markdown: true, page: page, ...params }
+    }).then(res => {
+      dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } })
+    }).catch(e => {
+      dispatch({ type: ACTIONS.ERROR, payload: { error: e } })
+    })
   }, [params, page])
 
-  return {
-    jobs: [],
-    loading: false,
-    error: false
-  }
+  return state
 }
