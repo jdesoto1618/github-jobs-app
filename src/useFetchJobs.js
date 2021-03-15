6c7340838119e6b4d47f2c34457e3fc8383b3a70
Joe DeSoto <joe.desoto@leaselabs.com>
2021-03-15 11:33:53 -0700

    Update: pulls data from custom hook into app.js bootstrap component.

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,4 +1,8 @@
 // hook for getting all the different yobs from Github
 export default function useFetchJobs(params, page) {
-  
+  return {
+    jobs: [],
+    loading: false,
+    error: false
+  }
 }
