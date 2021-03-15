bfba5908aa844258763f532fa8d18e397659f8bf
Joe DeSoto <joe.desoto@leaselabs.com>
2021-03-15 14:13:23 -0700

    Update: Adds jobs pagination component and adds bootstraps pagination to this.

---
+++ src/JobsPagination.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Pagination } from 'react-markdown';
+
+export default function JobsPagination({ page, setPage }) {
+  return (
+    <Pagination>
+      
+    </Pagination>
+  )
+}
