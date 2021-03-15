e8aee795a13a18d1a9b995c055ea6d548a220b82
Joe DeSoto <joe.desoto@leaselabs.com>
2021-03-15 13:18:26 -0700

    Update: adds job component and imports this into app.js.
    Uses this component within app.js to return a list of all
    jobs from the api.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap';
+import Job from './Job';
 
 function App() {
   const [params, setParams] = useState({})
@@ -11,7 +12,9 @@ function App() {
     <Container>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try Refreshing.</h1>}
-      <h1>{jobs.length}</h1>
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
     </Container>
   )
 }
