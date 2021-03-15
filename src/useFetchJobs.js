4486d3027c8f2f6f8d36884be74b6e8348224bc9
Joe DeSoto <joe.desoto@leaselabs.com>
2021-03-15 14:57:14 -0700

    Update: adds another axios request to handle if there is a next page to show.
    This is part of the pagination setup and also required the creation
    of a separate cancel token for this request.

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -31,10 +31,11 @@ export default function useFetchJobs(params, page) {
   const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
 
   useEffect(() => {
-    const cancelToken = axios.CancelToken.source()
+    const cancelToken1 = axios.CancelToken.source()
     dispatch({ type: ACTIONS.MAKE_REQUEST })
+    
     axios.get(BASE_URL, {
-      cancelToken: cancelToken.token,
+      cancelToken: cancelToken1.token,
       params: { markdown: true, page: page, ...params }
     }).then(res => {
       dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } })
@@ -43,8 +44,20 @@ export default function useFetchJobs(params, page) {
       dispatch({ type: ACTIONS.ERROR, payload: { error: e } })
     })
 
+    const cancelToken2 = axios.CancelToken.source()
+    axios.get(BASE_URL, {
+      cancelToken: cancelToken2.token,
+      params: { markdown: true, page: page + 1, ...params }
+    }).then(res => {
+      dispatch({ type: ACTIONS.UPDATE_HAS_NEXT_PAGE, payload: { hasNextPage: res.data.length !== 0 } })
+    }).catch(e => {
+      if (axios.isCancel(e)) return
+      dispatch({ type: ACTIONS.ERROR, payload: { error: e } })
+    })
+
     return () => {
-      cancelToken.cancel();
+      cancelToken1.cancel();
+      cancelToken2.cancel();
     }
   }, [params, page])
 
